439b0918cfb4b86c1e113ddd2cba8de5128c7348
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-05-12 21:30:30 -0300

    dados de usuário form

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
+import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   return (
     <>
       <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
       <DadosUsuario />
+      <DadosEntrega />
     </>
   );
 }
