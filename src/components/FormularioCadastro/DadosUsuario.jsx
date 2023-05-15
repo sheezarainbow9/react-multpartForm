c8f007d74d52b89a5a48f3e56d38635acc694f7c
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-05-15 10:23:04 -0300

    injeção de dependências

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { TextField, Button } from "@material-ui/core";
 
-function DadosUsuario() {
+function DadosUsuario({ aoEnviar }) {
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar();
+      }}
+    >
       <TextField
         id="email"
         label="email"
         type="email"
+        required
         variant="outlined"
         margin="normal"
         fullWidth
@@ -16,6 +22,7 @@ function DadosUsuario() {
         id="senha"
         label="senha"
         type="password"
+        required
         variant="outlined"
         margin="normal"
         fullWidth
