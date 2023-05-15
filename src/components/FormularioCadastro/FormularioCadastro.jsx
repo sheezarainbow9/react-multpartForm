2823159e2964f044577d8781f24ac12f29388207
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-05-15 10:01:07 -0300

    renderização condicional

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
+import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
-  return (
-    <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+  const [etapaAtual, setetapaAtual] = useState(1);
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />;
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>;
+    }
+  }
+
+  return <>{formularioAtual(etapaAtual)}</>;
 }
 
 export default FormularioCadastro;
