1715b3512f2f2c1089f726210e662bf7e4d25e04
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-05-16 12:20:41 -0300

    ValidacoesCadastro

--- src/contexts/ValidacoesCadastro.js
+++ src/contexts/ValidacoesCadastro.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const ValidacoesCadastro = React.createContext();
+// const ValidacoesCadastro = React.createContext();
 
-// Aqui seria com o valor default:
-// const ValidacoesCadastro = React.createContext({
-//   cpf: semValidacao,
-//   senha: semValidacao,
-//   nome: semValidacao,
-// });
+//Aqui seria com o valor default:
+const ValidacoesCadastro = React.createContext({
+  cpf: semValidacao,
+  senha: semValidacao,
+  nome: semValidacao,
+});
 
-// function semValidacao(dados) {
-//   console.log(dados);
-//   return { valido: true, texto: "" };
-// }
+function semValidacao(dados) {
+  console.log(dados);
+  return { valido: true, texto: "" };
+}
 
 export default ValidacoesCadastro;
