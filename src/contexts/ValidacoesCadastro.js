0b0c374046c35850ce6b42dd0a677812d4823bc4
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-05-16 11:18:58 -0300

    valor default

--- src/contexts/ValidacoesCadastro.js
+++ src/contexts/ValidacoesCadastro.js
@@ -2,4 +2,16 @@ import React from "react";
 
 const ValidacoesCadastro = React.createContext();
 
-export default ValidacoesCadastro
+// Aqui seria com o valor default:
+// const ValidacoesCadastro = React.createContext({
+//   cpf: semValidacao,
+//   senha: semValidacao,
+//   nome: semValidacao,
+// });
+
+// function semValidacao(dados) {
+//   console.log(dados);
+//   return { valido: true, texto: "" };
+// }
+
+export default ValidacoesCadastro;
